56e8d33eb41b2ed3f969410a3cc26af65f1f9f5d
daniela <daniela.moreno1@mail.udp.cl>
2021-09-29 23:02:00 -0300

    csv read the references

--- BlueScore.py
+++ BlueScore.py
@@ -1,17 +1,26 @@
 
 from nltk.translate.bleu_score import sentence_bleu
+import csv
 
 def BleuScore(text,referenceList):
-	print(referenceList)
 	reference = []
 	for sentence in referenceList:
 		separated = sentence.split()
-		print(separated)
 		reference.append(separated)
-	print(reference)
 	candidate = text.split()
-	print('BLEU score -> {}'.format(sentence_bleu(reference, candidate)))
+	return(sentence_bleu(reference, candidate))
 
 
-BleuScore("First test of fake data",["First test of real data","Second test of real data"])
-BleuScore("First test of real data",["First test of real data","Second test of real data"])
+
+referencesStringLen = 22  #numero de caracteres minimo de una referencia
+referenceLen = 100        #numero de referencias utilizadas
+referenceList = []        
+with open('corpus_good.csv', newline='') as File:  
+        reader = csv.reader(File)
+        for row in reader:
+                if(len(row[0]) >=  referencesStringLen):
+                        referenceList.append(row[0])
+                if(len(referenceList)==referenceLen):
+                        break
+print('BLEU score -> {}'.format(BleuScore("corona has become something else ugandan airport has misunderstood everything now extorting money from",referenceList)))
+print('BLEU score -> {}'.format(BleuScore("corona has become something else flowers airport has misunderstood everything now extorting money from",referenceList)))	     
